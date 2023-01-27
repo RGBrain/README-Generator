5f38b73e32c80b4da3df4fd772057a3ad8e506fe
RGBrain <31857067+RGBrain@users.noreply.github.com>
2023-01-27 18:44:58 +0000

    add question array

--- index.js
+++ index.js
@@ -5,7 +5,54 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    [
+        {
+          type: "input",
+          name: "GitHub",
+          message: "What is your GitHub username?"
+        },        
+        {
+          type: "input",
+          name: "Email",
+          message: "What is your email?"
+        },        
+        {
+          type: "input",
+          name: "ProjectName",
+          message: "What is the project name?"
+        },
+        {
+          type: "input",
+          name: "Description",
+          message: "Description:"
+        },
+        {
+          type: "input",
+          name: "Installation",
+          message: "Installation Instructions:"
+        },
+        {
+          type: "input",
+          name: "Usage",
+          message: "Usage Instructions:"
+        },
+        {
+          type: "input",
+          name: "Contribution",
+          message: "Contribution Guidelines:"
+        },
+        {
+          type: "input",
+          name: "Test",
+          message: "Test Instructions:"
+        },
+        {
+          type: "list",
+          name: "License",
+          message: "Which License would you like to use?",
+          choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+        }
+      ]
 ];
 
 // function to write README file
